e055dcbf0cc9b2a1ceb7c41fa6c157809dd1423c
amiralda <augustindany@hotmail.com>
2019-07-14 18:12:13 -0400

    First Commit

---
+++ app/src/main/java/com/example/flickster/MovieActivity.java
@@ -0,0 +1,65 @@
+package com.example.flickster;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Adapter;
+
+import com.example.flickster.adapters.MoviesAdapter;
+import com.example.flickster.models.Movie;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MovieActivity extends AppCompatActivity {
+
+    private static final String MOVIE_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    List<Movie> movies;
+   //private Object MoviesAdapter;
+   //private RecyclerView.Adapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView rvMovies = findViewById(R.id.rvMovies);
+        movies = new ArrayList<>();
+        final MoviesAdapter adapter = new MoviesAdapter(this, movies);
+        rvMovies.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        rvMovies.setAdapter(adapter);
+
+        AsyncHttpClient client =  new AsyncHttpClient();
+        client.get(MOVIE_URL, new JsonHttpResponseHandler(){
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONArray movieJsonArray = response.getJSONArray("results");
+                    movies.addAll(Movie.fromJsonArray(movieJsonArray));
+                    adapter.notifyDataSetChanged();
+                    Log.d("smile", movies.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
+    }
+}
